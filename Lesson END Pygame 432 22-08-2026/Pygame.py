0d545210ded2a5642e9d0f1526cc515c6ebe1665
OwenSouff <souff.owen@gmail.com>
2026-08-22 13:25:12 +0200

    Add files via upload

---
+++ Lesson END Pygame 432 22-08-2026/Pygame.py
@@ -0,0 +1,68 @@
+import pygame
+import os
+
+WIDTH = 900
+HEIGHT = 500
+WINDOW =pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Fight")
+
+WHITE = (255, 255, 255)
+FPS = 60
+VEL = 5
+SPACESHIP_WIDTH = 80
+SPACESHIP_HEIGHT = 60
+
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
+YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
+    YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
+
+RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_red.png'))
+RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
+    RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
+
+def yellow_control(keys_pressed, yellow):
+    if keys_pressed[pygame.K_a]:
+        yellow.x-=VEL
+    if keys_pressed[pygame.K_d]:
+        yellow.x+=VEL
+    if keys_pressed[pygame.K_w]:
+        yellow.y-=VEL
+    if keys_pressed[pygame.K_s]:
+        yellow.y+=VEL
+
+def red_control(keys_pressed, red):
+    if keys_pressed[pygame.K_LEFT]:
+        red.x-=VEL
+    if keys_pressed[pygame.K_RIGHT]:
+        red.x+=VEL
+    if keys_pressed[pygame.K_UP]:
+        red.y-=VEL
+    if keys_pressed[pygame.K_DOWN]:
+        red.y+=VEL
+
+def drawWindow(red, yellow):
+    WINDOW.fill(WHITE)
+    WINDOW.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
+    WINDOW.blit(RED_SPACESHIP, (red.x, red.y))
+    pygame.display.update()
+
+def main():
+    red = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+    yellow = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+    clock = pygame.time.Clock()
+    run =True
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        #yellow.x+=1
+        keys_pressed = pygame.key.get_pressed()
+        red_control(keys_pressed, red)
+        yellow_control(keys_pressed, yellow)
+        drawWindow(red, yellow)
+    pygame.quit()
+
+if __name__ == '__main__':
+    main()
+
